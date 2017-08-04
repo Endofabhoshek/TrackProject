0f16913b52206f42c748b53887d94c4934a42cf1
gurunaik111 <gurunaik111@gmail.com>
2017-08-05 00:32:27 +0530

    first commit

---
+++ Track1/app/src/main/java/com/example/guru/track1/MapScreen.java
@@ -0,0 +1,16 @@
+package com.example.guru.track1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by GuRu on 04/08/2017.
+ */
+
+public class MapScreen extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
